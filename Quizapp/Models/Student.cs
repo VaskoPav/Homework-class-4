e84d5786f8d2040edfa75512383033a45998867f
VaskoPav <79167640+VaskoPav@users.noreply.github.com>
2021-05-04 20:25:15 +0200

    Add files via upload

---
+++ Quizapp/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using QuizApp.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizApp.Models
+{
+    public class Student : User
+    {
+
+        public int Points { get; set; }
+
+        public Student()
+        {
+
+        }
+
+        public Student (string firstname, string lastname, string username, string password, Role role) : base(firstname, lastname, username, password, role)
+        {
+            Role = Role.Student;
+        }
+
+       
+    }
+}
---
+++ Quizapp/Models/Teacher.cs
@@ -0,0 +1,22 @@
+﻿using QuizApp.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizApp.Models
+{
+    public class Teacher:User
+    {
+        public Teacher()
+        {
+
+        }
+        public Teacher(string firstname, string lastname, string username, string password, Role role) : base(firstname, lastname, username, password, role)
+        {
+            Role = Role.Teacher;
+        }
+
+
+        
+    }
+}
