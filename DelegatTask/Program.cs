46eb0df4c079fa0da4ef99f8604ae0c0b36993a2
VaskoPav <79167640+VaskoPav@users.noreply.github.com>
2021-05-17 21:44:07 +0200

    Add files via upload

---
+++ DelegatTask/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DelegatTask
+{
+    class Program
+    {
+        public delegate bool FootballClubs(string word1,string word2);
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Func<string, string, bool> CompareClubs = (word1, word2) => word1.Length > word2.Length;
+           
+
+            FootballClubs compareClubsFootball = new FootballClubs(CompareClubs);
+            
+
+            StringMagic("Manchester", "Milan", compareClubsFootball);
+            
+
+
+
+            Console.ReadLine();
+        }
+        
+
+        private static void StringMagic(string v1, string v2, FootballClubs compareClubsFootball)
+        {
+            Console.WriteLine($"This club {v1} is bigger than {v2}");
+        }
+    }
+}
