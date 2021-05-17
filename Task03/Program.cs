2720962eff4d35fed3cc05965d05a54bc300775b
VaskoPav <79167640+VaskoPav@users.noreply.github.com>
2021-05-17 21:41:55 +0200

    Add files via upload

---
+++ Task03/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Task01.Entiteis;
+using Task03.Entities;
+
+namespace Task03
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+           
+
+            List<string> strings = new List<string>() { "My", "Generics", "Method" };
+            List<int> integers = new List<int>() { 1, 2, 3, 4, 5 };
+            List<bool> booleans = new List<bool>() { true, true, false, true, false };
+
+            Console.WriteLine("========================================================");
+
+            GenericsMethods.SeeItems(strings);
+            GenericsMethods.SeeThis(strings);
+
+            Console.WriteLine("========================================================");
+
+            GenericsMethods.SeeItems(integers);
+            GenericsMethods.SeeThis(integers);
+
+            Console.WriteLine("========================================================");
+
+            GenericsMethods.SeeItems(booleans);
+            GenericsMethods.SeeThis(booleans);
+
+            Console.WriteLine("========================================================");
+
+           
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
