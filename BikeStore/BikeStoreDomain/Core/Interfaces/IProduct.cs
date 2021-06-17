21901e78b273f162a11bafd2dd834fa6ab7b7e17
VaskoPav <79167640+VaskoPav@users.noreply.github.com>
2021-06-17 14:17:49 +0200

    Add files via upload

---
+++ BikeStore/BikeStoreDomain/Core/Interfaces/IProduct.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BikeStoreDomain.Core.Interfaces
+{
+    public interface IProduct
+    {
+        string NameProduct { get; set; }
+        int Price { get; set; }
+        int RamBike { get; set; }
+    }
+}
