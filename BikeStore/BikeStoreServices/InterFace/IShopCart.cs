ce444770cb51ed57d34be3313091c9b03537696e
VaskoPav <79167640+VaskoPav@users.noreply.github.com>
2021-06-18 10:48:25 +0200

    Add files via upload

---
+++ BikeStore/BikeStoreServices/InterFace/IShopCart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BikeStoreServices.InterFace
+{
+    public interface IShopCart
+    {
+        void ListAllProduct();
+        void CreateOrder();
+        void RemoveOrder();
+    }
+}
